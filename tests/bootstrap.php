811fa195e41e2bf75e3c1cc46f3bfa9c3e8fbd65
Paweł Sroka <srokap@gmail.com>
2013-05-14 12:27:21 +0200

    Added Travis API credentials directly to tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 require_once('PHPUnit/Autoload.php');
-require_once(dirname(dirname(__FILE__)).'/config.php');
+
+// require_once(dirname(dirname(__FILE__)).'/config.php');
+define('eP_API_KEY', '6c627a9694f43fe87ac1bed7e02e7fa5');
+define('eP_API_SECRET', '72a2e23c6730d81edd61710ae6050a72');
+
 require_once(dirname(dirname(__FILE__)).'/ep_API.php');
